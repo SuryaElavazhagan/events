37851d0175dceded47db5e63396508430faa5099
surya.e <surya.e@zohocorp.com>
2020-07-07 23:40:20 +0530

    update: added more options for events > need to implement actions

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/components/event-card.scss';
 import { Event } from '../../model/Event';
 import { format } from 'date-fns';
@@ -7,7 +7,22 @@ interface EventCardProps {
   event: Event;
 }
 
+type EventAction = 'rename' | 'delete' | 'download' | 'share';
+
 export function EventCard({ event }: EventCardProps) {
+  const [showOptions, setShowOptions] = useState(false);
+
+  function toggleOptions() {
+    setShowOptions(!showOptions);
+  }
+
+  function handleAction(action: EventAction) {
+    switch(action) {
+      default:
+        throw new Error('Method not implemented yet!');
+    }
+  }
+  
   return (
     <div className="event-card">
       <img
@@ -16,12 +31,27 @@ export function EventCard({ event }: EventCardProps) {
         alt="Event Poster"
       />
       <div className="card event-details">
-        <p title={event.name} className="event-name">{ event.name }</p>
-        <p className="event-stats">
-          on { format(event.date, 'dd MMM, yyyy') }
-          <span className="event-stats-separator"></span>
-          { event.views } views
-        </p>
+        <div className="card-content">
+          <p title={event.name} className="event-name">{ event.name }</p>
+          <p className="event-stats">
+            on { format(event.date, 'dd MMM, yyyy') }
+            <span className="event-stats-separator"></span>
+            { event.views } views
+          </p>
+        </div>
+        <svg onClick={toggleOptions} className="event-options-trigger" width="24" height="24">
+          <use xlinkHref="/sprite.svg#more" />
+        </svg>
+        {
+          showOptions ? (
+            <div className="event-options">
+              <span onClick={() => handleAction('rename')}>Rename</span>
+              <span onClick={() => handleAction('share')}>Share</span>
+              <span onClick={() => handleAction('download')}>Download</span>
+              <span onClick={() => handleAction('delete')} className="delete">Delete</span>
+            </div>
+          ) : undefined
+        }
       </div>
     </div>
   );
